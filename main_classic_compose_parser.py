4fac12cd8db95c701c55e3d5c6ca5d29044ad5b3
rutesun <rutesun@gmail.com>
2025-11-19 00:38:31 +0900

    refactor to using langgraph

--- main_classic_compose_parser.py
+++ main_classic_compose_parser.py
@@ -1,48 +1,34 @@
-from langchain_classic import hub
-from langchain_classic.agents import AgentExecutor
-from langchain_classic.agents.react.agent import create_react_agent
-from langchain_core.output_parsers.pydantic import PydanticOutputParser
-from langchain_core.prompts import PromptTemplate
-from langchain_core.runnables import RunnableLambda
+from dotenv import load_dotenv
+from langchain.agents import create_agent
+from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
 from langchain_tavily import TavilySearch
 
-from prompt import REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS
 from schemas import AgentResponse
 
-from dotenv import load_dotenv
 load_dotenv()
 
 tools = [TavilySearch()]
 llm = ChatOpenAI(model="gpt-4")
-structured_llm = llm.with_structured_output(AgentResponse)
 
-react_prompt = hub.pull("hwchase17/react")
-output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
-react_prompt_with_format_instructions = PromptTemplate(
-    template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
-    input_variables=["input", "agent_scratchpad", "tool_names"],
-).partial(format_instructions="")
-
-
-agent = create_react_agent(
-    llm=llm,
+agent = create_agent(
+    model=llm,
     tools=tools,
-    # prompt=react_prompt,
-    prompt=react_prompt_with_format_instructions,
+    response_format=AgentResponse,
 )
-
-agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
-chain = agent_executor
-extract_output = RunnableLambda(lambda x: x["output"])
-# parse_output = RunnableLambda(lambda x: output_parser.parse(x))
-chain = agent_executor | extract_output | structured_llm
 
 
 def main():
-    result = chain.invoke(        
-                          input={            
-                                 "input": "search Top 5 news in USA stock market today",        }    )    
+    result = agent.invoke(
+        {
+            "messages": [
+                HumanMessage(
+                    content="search Top 5 news in USA stock market today"
+                )
+            ]
+        }
+    )
     print(result)
+    
 if __name__ == "__main__":  
     main()
